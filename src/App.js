7963e4f8b4b448bcfe7085f468f5a1e25aaaedfb
Ivan1990Nik <pelevin.vanyusha90@mail.ru>
2024-06-09 20:39:13 +0500

    social network

--- src/App.js
+++ src/App.js
@@ -4,26 +4,31 @@ import './App.css';
 
 const App = () => {
   return (
-<div>
-  <Header />
-  <ul className='app'>
-    <li>HTLM</li>
-    <li>CSS</li>
-    <li>JS</li>
-    <li>JSX</li>
-  </ul>
-</div >
-  );
-}
+    <div className='app-wraper'>
+      <header className='header'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ7fTFb3G8JZJy_oWg4rfoketLxdUnUF2eLw&usqp=CAU" />
+      </header>
+      <nav className='nav'>
+        <div><a>Profile</a></div>
+        <div><a>Messeges</a></div>
+        <div><a>News</a></div>
+        <div><a>Music</a></div>
+        <div><a>Settings</a></div>
+      
+      </nav>
+      <div className='content'>
+        <div>
+          <img src="https://kartinki.pics/uploads/posts/2021-03/thumbs/1616115428_4-p-vesna-krasivie-foto-5.jpg"/>
+        </div>
+        <div>Ava + Description</div>
+        <div>My posts
+          <div>New post</div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div >
 
-
-const Header = () => {
-  return (
-    <ul>
-      <a href="#s">a</a>
-      <a href="#s">b</a>
-      <a href="#s">c</a>
-    </ul>
   )
 };
 export default App;
